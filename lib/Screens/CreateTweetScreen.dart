53f2eb3cd4ee59d24945838c7d678348f4bf913f
TaishiU <nbadreamteam3@gmail.com>
2021-08-17 21:00:40 +0900

    ツイート投稿の画像選択(複数)

--- lib/Screens/CreateTweetScreen.dart
+++ lib/Screens/CreateTweetScreen.dart
@@ -1,14 +1,8 @@
 import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:twitter_clone/Constants/Constants.dart';
-import 'package:twitter_clone/Firebase/Firestore.dart';
-import 'package:twitter_clone/Firebase/Storage.dart';
-import 'package:twitter_clone/Model/Tweet.dart';
-import 'package:twitter_clone/Model/User.dart';
-import 'package:twitter_clone/Widget/RoundedButton.dart';
 
 class CreateTweetScreen extends StatefulWidget {
   final String currentUserId;
@@ -20,9 +14,9 @@ class CreateTweetScreen extends StatefulWidget {
 }
 
 class _CreateTweetScreenState extends State<CreateTweetScreen> {
-  late String _tweetText;
-  File? _tweetImage;
-  bool _isLoading = false;
+  //late String _tweetText;
+  List<File> _tweetImage = [];
+  //bool _isLoading = false;
   final _formKey = GlobalKey<FormState>();
 
   handleImageFromGallery() async {
@@ -31,7 +25,7 @@ class _CreateTweetScreenState extends State<CreateTweetScreen> {
           await ImagePicker().getImage(source: ImageSource.gallery);
       if (imageFile != null) {
         setState(() {
-          _tweetImage = File(imageFile.path);
+          _tweetImage.add(File(imageFile.path));
         });
       }
     } catch (e) {
@@ -39,45 +33,45 @@ class _CreateTweetScreenState extends State<CreateTweetScreen> {
     }
   }
 
-  handleTweet() async {
-    _formKey.currentState!.save();
-    if (_formKey.currentState!.validate() && !_isLoading) {
-      setState(() {
-        _isLoading = true;
-      });
-      String tweetImageUrl;
-      bool hasImage;
-      if (_tweetImage == null) {
-        tweetImageUrl = '';
-        hasImage = false;
-      } else {
-        tweetImageUrl = await Storage().uploadTweetImage(
-            userId: widget.currentUserId, imageFile: _tweetImage!);
-        hasImage = true;
-      }
-
-      DocumentSnapshot userProfileDoc =
-          await Firestore().getUserProfile(userId: widget.currentUserId);
-      User user = User.fromDoc(userProfileDoc);
-
-      Tweet tweet = Tweet(
-        authorName: user.name,
-        authorId: user.userId,
-        authorProfileImage: user.profileImage,
-        text: _tweetText,
-        image: tweetImageUrl,
-        hasImage: hasImage,
-        timestamp: Timestamp.fromDate(DateTime.now()),
-        likes: 0,
-        reTweets: 0,
-      );
-      Firestore().createTweet(tweet: tweet);
-      Navigator.of(context).pop();
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
+  // handleTweet() async {
+  //   _formKey.currentState!.save();
+  //   if (_formKey.currentState!.validate() && !_isLoading) {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     String tweetImageUrl;
+  //     bool hasImage;
+  //     if (_tweetImage == null) {
+  //       tweetImageUrl = '';
+  //       hasImage = false;
+  //     } else {
+  //       tweetImageUrl = await Storage().uploadTweetImage(
+  //           userId: widget.currentUserId, imageFile: _tweetImage!);
+  //       hasImage = true;
+  //     }
+  //
+  //     DocumentSnapshot userProfileDoc =
+  //         await Firestore().getUserProfile(userId: widget.currentUserId);
+  //     User user = User.fromDoc(userProfileDoc);
+  //
+  //     Tweet tweet = Tweet(
+  //       authorName: user.name,
+  //       authorId: user.userId,
+  //       authorProfileImage: user.profileImage,
+  //       text: _tweetText,
+  //       image: tweetImageUrl,
+  //       hasImage: hasImage,
+  //       timestamp: Timestamp.fromDate(DateTime.now()),
+  //       likes: 0,
+  //       reTweets: 0,
+  //     );
+  //     Firestore().createTweet(tweet: tweet);
+  //     Navigator.of(context).pop();
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -95,18 +89,20 @@ class _CreateTweetScreenState extends State<CreateTweetScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: SingleChildScrollView(
-          /*キーボード表示時のRenderFlexOverFlowedエラーの解消用にSingleChildScrollViewを使う*/
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(height: 20),
-                TextFormField(
+      body: SingleChildScrollView(
+        /*キーボード表示時のRenderFlexOverFlowedエラーの解消用にSingleChildScrollViewを使う*/
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  /* keyboardTypeとmaxLinesを上記のように指定することでテキスト折り返しが可能になる */
                   maxLength: 140,
-                  maxLines: 8,
                   decoration: InputDecoration(
                     hintText: 'Enter your Tweet',
                   ),
@@ -114,61 +110,102 @@ class _CreateTweetScreenState extends State<CreateTweetScreen> {
                       ? 'Please enter your Tweet'
                       : null,
                   onChanged: (value) {
-                    _tweetText = value;
+                    // _tweetText = value;
                   },
                 ),
-                SizedBox(height: 10),
-                _tweetImage == null
-                    ? SizedBox.shrink()
-                    : Column(
-                        children: [
-                          Container(
-                            height: 200,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: TwitterColor,
-                              image: DecorationImage(
-                                image: FileImage(_tweetImage!),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                SizedBox(height: 10),
-                GestureDetector(
-                  onTap: () {
-                    handleImageFromGallery();
-                  },
-                  child: Container(
-                    height: 70,
-                    width: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      border: Border.all(
-                        color: TwitterColor,
-                        width: 2,
-                      ),
-                    ),
-                    child: Icon(
-                      Icons.camera_alt,
-                      size: 50,
+              ),
+              // _tweetImage == null
+              //     ? SizedBox.shrink()
+              //     : Column(
+              //         children: [
+              //           Container(
+              //             height: 200,
+              //             decoration: BoxDecoration(
+              //               borderRadius: BorderRadius.circular(20),
+              //               color: TwitterColor,
+              //               image: DecorationImage(
+              //                 image: FileImage(_tweetImage!),
+              //                 fit: BoxFit.cover,
+              //               ),
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {
+                  handleImageFromGallery();
+                },
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    border: Border.all(
                       color: TwitterColor,
+                      width: 2,
                     ),
                   ),
+                  child: Icon(
+                    Icons.camera_alt,
+                    size: 50,
+                    color: TwitterColor,
+                  ),
                 ),
-                SizedBox(height: 20),
-                RoundedButton(
-                  btnText: 'Tweet',
-                  onBtnPressed: () {
-                    handleTweet();
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _tweetImage.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 4),
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                              image: FileImage(_tweetImage[index]),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 10,
+                          right: 15,
+                          child: Container(
+                            width: 35,
+                            height: 35,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.black45,
+                            ),
+                            child: Icon(
+                              Icons.clear_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
                   },
                 ),
-                SizedBox(height: 5),
-                _isLoading ? CircularProgressIndicator() : SizedBox.shrink(),
-              ],
-            ),
+              ),
+              // SizedBox(height: 20),
+              // RoundedButton(
+              //   btnText: 'Tweet',
+              //   onBtnPressed: () {
+              //     handleTweet();
+              //   },
+              // ),
+              // SizedBox(height: 5),
+              // _isLoading ? CircularProgressIndicator() : SizedBox.shrink(),
+            ],
           ),
         ),
       ),
