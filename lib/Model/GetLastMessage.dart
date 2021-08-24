c35a3a53f6bfb3c105e470a565ce87dac54e1002
TaishiU <nbadreamteam3@gmail.com>
2021-08-24 16:10:50 +0900

    チャット履歴表示

---
+++ lib/Model/GetLastMessage.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GetLastMessage {
+  final String content;
+  final String? convoId;
+  final String idFrom;
+  final String idTo;
+  final bool read;
+  final Timestamp timestamp;
+  final String user1Id;
+  final String user1Name;
+  final String user1ProfileImage;
+  final String user2Id;
+  final String user2Name;
+  final String user2ProfileImage;
+  final String userFrom;
+  final String userTo;
+
+  GetLastMessage({
+    required this.content,
+    this.convoId,
+    required this.idFrom,
+    required this.idTo,
+    required this.read,
+    required this.timestamp,
+    required this.user1Id,
+    required this.user1Name,
+    required this.user1ProfileImage,
+    required this.user2Id,
+    required this.user2Name,
+    required this.user2ProfileImage,
+    required this.userFrom,
+    required this.userTo,
+  });
+
+  factory GetLastMessage.fromDoc(DocumentSnapshot doc) {
+    return GetLastMessage(
+      content: doc['content'],
+      convoId: doc['convoId'],
+      idFrom: doc['idFrom'],
+      idTo: doc['idTo'],
+      read: doc['read'],
+      timestamp: doc['timestamp'],
+      user1Id: doc['user1Id'],
+      user1Name: doc['user1Name'],
+      user1ProfileImage: doc['user1ProfileImage'],
+      user2Id: doc['user2Id'],
+      user2Name: doc['user2Name'],
+      user2ProfileImage: doc['user2ProfileImage'],
+      userFrom: doc['userFrom'],
+      userTo: doc['userTo'],
+    );
+  }
+}
--- lib/Screens/ChatScreen.dart
+++ lib/Screens/ChatScreen.dart
@@ -1,10 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/Constants/Constants.dart';
+import 'package:twitter_clone/Firebase/Firestore.dart';
+import 'package:twitter_clone/Model/GetLastMessage.dart';
+import 'package:twitter_clone/Model/User.dart';
+import 'package:twitter_clone/Screens/MessageScreen.dart';
 import 'package:twitter_clone/Screens/SelectChatUser.dart';
 
 class ChatScreen extends StatelessWidget {
   final String currentUserId;
   ChatScreen({Key? key, required this.currentUserId}) : super(key: key);
+
+  moveToChatScreen({
+    required BuildContext context,
+    required String convoId,
+    required String peerUserId,
+  }) async {
+    /*相手ユーザーのプロフィール*/
+    DocumentSnapshot userProfileDoc =
+        await Firestore().getUserProfile(userId: peerUserId);
+    User peerUser = User.fromDoc(userProfileDoc);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MessageScreen(
+          currentUserId: currentUserId,
+          convoId: convoId,
+          peerUser: peerUser,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +60,107 @@ class ChatScreen extends StatelessWidget {
             },
           ),
         ],
+      ),
+      body: StreamBuilder(
+        stream: messagesRef
+            .orderBy('timestamp', descending: true)
+            .where('users', arrayContains: currentUserId)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          List<DocumentSnapshot> listLastMessagesSnap = snapshot.data!.docs;
+          return ListView(
+            physics: BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics(),
+            ),
+            children: listLastMessagesSnap.map((message) {
+              GetLastMessage getLastMessage = GetLastMessage.fromDoc(message);
+              final _isOwner = currentUserId == getLastMessage.user1Id;
+              /*user1Idがユーザー自身のidと一致するか*/
+              return Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      moveToChatScreen(
+                        context: context,
+                        convoId: getLastMessage.convoId!,
+                        peerUserId: _isOwner
+                            ? getLastMessage.user2Id
+                            : getLastMessage.user1Id,
+                      );
+                    },
+                    child: Container(
+                      /*未読状態のメッセージには背景色(青色)を設定する*/
+                      color: !getLastMessage.read &&
+                              getLastMessage.idTo == currentUserId
+                          ? Colors.yellow
+                          : Colors.transparent,
+                      height: 80,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Container(
+                          color: Colors.transparent,
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 25,
+                                backgroundColor: Colors.blue,
+                                backgroundImage: _isOwner
+                                    ? NetworkImage(
+                                        getLastMessage.user2ProfileImage)
+                                    : NetworkImage(
+                                        getLastMessage.user1ProfileImage),
+                              ),
+                              SizedBox(width: 20),
+                              Container(
+                                color: Colors.transparent,
+                                width: MediaQuery.of(context).size.width * 0.69,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          _isOwner
+                                              ? getLastMessage.user2Name
+                                              : getLastMessage.user1Name,
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                        SizedBox(height: 5),
+                                        Text(getLastMessage.content),
+                                      ],
+                                    ),
+                                    Text(
+                                      '${getLastMessage.timestamp.toDate().month.toString()}/${getLastMessage.timestamp.toDate().day.toString()}',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              //SizedBox(width: 20),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                ],
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
